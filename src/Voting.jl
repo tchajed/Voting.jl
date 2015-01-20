b9b792ff7eddd09cd3301185b19dd56e20cca96b
Tej Chajed <tchajed@gmail.com>
2015-01-20 09:48:14 -0500

    Add an initial implementation

--- src/Voting.jl
+++ src/Voting.jl
@@ -1,5 +1,182 @@
 module Voting
 
-# package code goes here
+export  Vote,
+        Votes,
+        Ballot,
+        vote,
 
-end # module
+        pareto_dominates,
+        condorcet,
+
+        plurality,
+        plurality_with_elimination,
+        borda,
+        pairwise_elimination
+
+import Base: length, start, done, next, show
+
+type Vote
+    order::Array{Int64, 1}
+    weight::Int64
+end
+
+# Iterate over order
+start(vote::Vote) = 1
+done(vote::Vote, index) = index >= length(vote.order)
+next(vote::Vote, index) = (vote.order[index], index+1)
+
+# Construction
+vote(order::Array{Int64, 1}) = Vote(order, 1)
+vote(order::Int64...) = Vote(collect(order), 1)
+*(v::Vote, count::Int) = Vote(v.order, v.weight * count)
+*(count::Int, v::Vote) = v * count
+
+getrank(v::Vote, a) = findfirst(v.order, a)
+prefers(v::Vote, a, b) = getrank(v, a) < getrank(v, b)
+
+function delete(v::Vote, a)
+    order = map(v.order[v.order .!= a]) do c
+        c > a ? c-1 : c
+    end
+    Vote(order, v.weight)
+end
+
+type Ballot{T}
+    candidates::Array{T, 1}
+end
+
+getindex(b::Ballot, v::Vote) = b.candidates[v.order]
+getindex(b::Ballot, i) = b.candidates[i]
+
+type Votes{T}
+    ballot::Ballot{T}
+    votes::Array{Vote, 1}
+end
+
+function show(io::IO, votes::Votes)
+    for v in votes
+        v_candidates = votes.ballot[v.order]
+        print(io, "$(v.weight)× $(v_candidates)\n")
+    end
+end
+
+length(v::Vote) = length(v.order)
+length(b::Ballot) = length(b.candidates)
+
+function Votes{T}(b::Ballot{T}, votes::Array{Vote, 1})
+    l = length(b)
+    for v in votes
+        if v.order != unique(v.order)
+            throw(ArgumentError("order $(v.order) is not a permutation"))
+        end
+        if length(v) != l
+            throw(ArgumentError("unexpected vote of length $(length(v)) != $l"))
+        end
+    end
+    return Votes{T}(b, votes)
+end
+
+# TODO(tchajed): provide a conversion (convert) from Array{T, 1} to Ballot{T}
+
+function Votes{T}(candidates::Array{T, 1}, votes...)
+    Votes(Ballot(candidates), collect(votes))
+end
+
+function Votes{T}(candidates::Array{T, 1}, a::Array{Int64, 2})
+    m = size(a, 1)
+    votes = []
+    for row in 1:m
+        ordering = collect(a[row,:])
+        push!(votes, vote(ordering))
+    end
+    return Votes(candidates, votes...)
+end
+
+length{T}(votes::Votes{T}) = length(votes.votes)
+start(votes::Votes) = 1
+done(votes::Votes, index) = index > length(votes.votes)
+next(votes::Votes, index) = (votes.votes[index], index+1)
+
+function delete(votes::Votes, i)
+    n = length(votes.ballot)
+    selection = [1:i-1, i+1:n]
+    return Votes(Ballot(votes.ballot[selection]),
+    [delete(v, i) for v in votes])
+end
+
+function plurality{T}(votes::Votes{T})
+    n = length(votes.ballot)
+    counts = zeros(n)
+    for v in votes
+        counts[first(v)] += v.weight
+    end
+    winners = find(counts .== maximum(counts))
+    return votes.ballot[winners]
+end
+
+function plurality_with_elimination{T}(votes::Votes{T})
+    n = length(votes.ballot)
+    if n <= 2
+        return plurality(votes)
+    end
+    counts = zeros(n)
+    for v in votes
+        counts[first(v)] += v.weight
+    end
+    loser = indmin(counts)
+    return plurality_with_elimination(delete(votes, loser))
+end
+
+# TODO(tchajed): handle ties in choice functions that use this
+function prefer{T}(votes::Votes{T}, a, b)
+    count_a = 0
+    count_b = 0
+    for v in votes
+        if prefers(v, a, b)
+            count_a += v.weight
+        else
+            count_b += v.weight
+        end
+    end
+    return count_a > count_b
+end
+
+function pareto_dominates{T}(votes::Votes{T}, a, b)
+    ai = findfirst(votes.ballot, a)
+    bi = findfirst(votes.ballot, b)
+    all(v -> prefers(v, ai, bi), votes)
+end
+
+function pairwise_elimination{T}(votes::Votes{T}, agenda::Array{T, 1})
+    indices = [findfirst(votes.ballot, c) for c in agenda]
+    while length(indices) > 1
+        a = indices[1]
+        b = indices[2]
+        winner = prefer(votes, a, b) ? a : b
+        indices = [winner, indices[3:end]]
+    end
+    return [votes.ballot[indices[1]]]
+end
+
+function borda{T}(votes::Votes{T})
+    n = length(votes.ballot)
+    counts = zeros(n)
+    for v in votes, c in 1:n
+        borda_count = (n - getrank(v, c)) * v.weight
+        counts[c] += borda_count
+    end
+    winners = find(counts .== maximum(counts))
+    return votes.ballot[winners]
+end
+
+function condorcet{T}(votes::Votes{T})
+    n = length(votes.ballot)
+    for c in 1:n
+        if all(o -> prefer(votes, c, o), [1:c-1, c+1:n])
+            return votes.ballot[[c]]
+        end
+    end
+    return []
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,70 @@
 using Voting
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Examples taken from Coursera "Game Theory II: Advanced Applications" course
+# from Stanford/UBC.
+
+unit12 = Votes([:A, :B, :C, :D],
+       [2 3 1 4;
+       2 4 3 1;
+       4 3 1 2;
+       1 4 2 3;
+       1 4 3 2])
+@test [] == condorcet(unit12)
+@test [:A, :B] == sort(plurality(unit12))
+@test [:D] == borda(unit12)
+
+paradox = Votes([:A, :B, :C],
+        499 * vote(1, 2, 3),
+        3 * vote(2, 3, 1),
+        498 * vote(3, 2, 1))
+@test [:B] == condorcet(paradox)
+@test [:A] == plurality(paradox)
+@test [:C] == plurality_with_elimination(paradox)
+
+iia_paradox = Votes([:A, :B, :C],
+        35 * vote(1, 3, 2),
+        33 * vote(2, 1, 3),
+        32 * vote(3, 2, 1))
+@test [:A] == plurality(iia_paradox)
+@test [:A] == borda(iia_paradox)
+
+iia_paradox_without_c = Voting.delete(iia_paradox, 3)
+@test [:B] == plurality(iia_paradox_without_c)
+@test [:B] == borda(iia_paradox_without_c)
+
+@test [:C] == pairwise_elimination(iia_paradox, [:A, :B, :C])
+@test [:B] == pairwise_elimination(iia_paradox, [:A, :C, :B])
+@test [:A] == pairwise_elimination(iia_paradox, [:B, :C, :A])
+
+pe_paradox = Votes([:A, :B, :C, :D],
+        [2 4 3 1;
+        1 2 4 3;
+        3 1 2 4])
+@test [:D] == pairwise_elimination(pe_paradox, [:A, :B, :C, :D])
+# :B Pareto-dominates :D
+@test pareto_dominates(pe_paradox, :B, :D)
+
+unit13 = Votes([:A, :B, :C, :D],
+        [2 3 1 4;
+        2 4 3 1;
+        4 3 1 2;
+        1 4 2 3;
+        1 4 3 2])
+@test [:D] == pairwise_elimination(unit13, [:A, :B, :C, :D])
+@test [:A] == pairwise_elimination(unit13, [:D, :C, :B, :A])
+candidates = unit13.ballot.candidates
+for first in candidates, other in candidates
+    @test !pareto_dominates(unit13, first, other)
+end
+
+unit16first = Votes([:a, :b, :c],
+    3 * vote(1, 2, 3),
+    2 * vote(2, 3, 1),
+    2 * vote(3, 2, 1))
+@test [:a] == plurality(unit16first)
+unit16second = Votes([:a, :b, :c],
+    3 * vote(1, 2, 3),
+    2 * vote(2, 3, 1),
+    2 * vote(2, 1, 3))
+@test [:b] == plurality(unit16second)
